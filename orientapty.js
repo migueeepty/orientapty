4911d2b63671db9aba49dd3cccdca21228c8978f
migueeepty <miguel.zaldivar@up.ac.pa>
2025-05-09 15:51:18 -0500

    Add files via upload

---
+++ orientapty.js
@@ -0,0 +1,4 @@
+function openBotpressChat() {
+  const iframeContainer = document.getElementById('botpress-iframe-container');
+  iframeContainer.style.display = iframeContainer.style.display === 'none' ? 'block' : 'none';
+}
